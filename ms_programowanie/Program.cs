f440d192f719d171e125d9532b76ca10027cab02
mateusz.swieszek <mateusz.swieszek@PK5-09>
2021-10-16 13:21:07 +0200

    Dodaj pliki projektów.

---
+++ ms_programowanie/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ms_programowanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int wylosowano = rnd.Next(0, 10);
+             
+            Console.WriteLine(wylosowano);
+            int podano = Convert.ToInt32(Console.ReadLine());
+            if (wylosowano == podano)
+            {
+                Console.WriteLine("trafiłeś");
+            }
+            else
+            {
+                Console.WriteLine("Jeszcze raz");
+            }
+        }
+    }
+}
